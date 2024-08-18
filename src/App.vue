d7e060a890e8c942adfec3f7d1a502e617f24d11
Muhammad-rahmat <muhammad22rahmathidayat@gmail.com>
2024-08-18 21:49:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>RHShop</h1>
+    <p class="animate__animated animate__fadeInRight">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente laborum
+      nihil atque, aut cum vitae, iste iusto accusamus aliquid obcaecati nemo,
+      temporibus nostrum magnam aperiam accusantium nesciunt dolorum dicta nisi!
+    </p>
+    <product-list
+      v-if="products.length > 0"
+      :products="products"
+      :maximum="50"
+    ></product-list>
+    <p v-else>Loading products...</p>
+    <!-- <Product-list :product="products" :maximum="maximum"></Product-list> -->
+  </div>
+</template>
+
+<script>
+import ProductList from "./components/ProductList.vue";
+
+export default {
+  name: "app",
+  data: function () {
+    return {
+      maximum: 50,
+      products: [],
+    };
+  },
+  components: {
+    ProductList,
+  },
+  mounted() {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+};
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,92 @@
+<template>
+  <transition-group
+    name="fade"
+    tag="div"
+    @before-enter="before"
+    @enter="enter"
+    @leave="leave"
+  >
+    <div
+      class="row d-none mb-3 align-items-center"
+      v-for="(item, index) in showItem"
+      :key="item.id"
+      :data-index="index"
+    >
+      <div class="col-1 m-auto">
+        <button class="btn btn-info" @click="$emit('add', item)">+</button>
+      </div>
+      <div class="col-sm-4">
+        <img :src="item.image" :alt="item.name" class="img-fluid d-block" />
+      </div>
+      <div class="col">
+        <h3 class="text-info">{{ item.name }}</h3>
+        <p class="mb-0">{{ item.description }}</p>
+        <div class="h5 float-right">
+          <price :value="Number(item.price)"></price>
+        </div>
+      </div>
+    </div>
+  </transition-group>
+</template>
+
+<script>
+import Price from "./PriceComponent.vue";
+
+export default {
+  name: "product-list",
+  components: {
+    Price,
+  },
+  props: {
+    products: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+    maximum: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    showItem() {
+      // Pastikan 'products' adalah array sebelum mencoba memfilter
+      if (Array.isArray(this.products)) {
+        return this.products.filter((item) => item.price <= this.maximum);
+      }
+      return [];
+    },
+  },
+  methods: {
+    before(el) {
+      el.className = "d-none";
+    },
+    enter(el) {
+      const delay = el.dataset.index * 100;
+      setTimeout(() => {
+        el.className =
+          "row d-flex mb-3 align-items-center animate__animated animate__bounceIn";
+      }, delay);
+    },
+    leave(el) {
+      const delay = el.dataset.index * 100;
+      setTimeout(() => {
+        el.className =
+          "row d-flex mb-3 align-items-center animate__animated animate__bounceOut";
+      }, delay);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
